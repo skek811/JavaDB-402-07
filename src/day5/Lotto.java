af1a24fde2dc86549b0cc78f63c16af223e1ac8e
Unknown <skek811@naver.com>
2018-08-01 17:16:39 +0900

    로또 완료

--- src/day5/Lotto.java
+++ src/day5/Lotto.java
@@ -13,16 +13,19 @@ public class Lotto {
 			System.out.println("번호 생성 실패");
 		}
 		else{
+			sort(arr,size);
 			output(arr);
 		}
 		arr2 = createRandomArray(min, max, size+1);
 		if(arr2==null){
-			System.out.println("번호 생성 실패");
+			System.out.println("당첨번호 생성 실패");
 		}
 		else{
-			System.out.print("당첨번호 + 보너스번호 : ");
+			sort(arr2,size);
 			output(arr2);
 		}
+		System.out.println(countCheck(arr,arr2));
+		printRank(arr,arr2);
 	}
 	public static boolean isDuplicate(int arr[], int num, int cnt){
 		if(cnt>arr.length){
@@ -71,7 +74,7 @@ public class Lotto {
 		int arr[] = new int[size];
 		
 		int cnt = 0;
-		for( ; cnt< arr.length; ){// arr.length = size
+		for( ; cnt< size; ){// arr.length = size
 			int tmp = randomInt(min, max);
 			if(!isDuplicate(arr, tmp, cnt)){
 				arr[cnt] = tmp;
@@ -85,8 +88,84 @@ public class Lotto {
 			System.out.print(arr[i]+ " ");
 		}*/ //일반 for문
 		for(int tmp:arr){
-			System.out.print(tmp + " ");
+			//System.out.print(tmp + " ");
+			System.out.printf("%2d ", tmp);//서식문자를 이용한 출력
 		}// 향상된for문 -> 읽기는 되지만 쓰기는 안된다.
 		System.out.println();
+	}
+	/* 두 배열이 주어졌을때 같은 번호가 몇개 있는지 반환하는 메소드를 만드세요
+	 * 메소드명 : countCheck
+	 * 매개변수 : int arr[], int arr2[]
+	 * 리턴타입 : int
+	 * arr와 arr2가 같으면 cnt를 증가시킨다. 
+	 */
+	public static int countCheck(int arr[], int arr2[]){
+		int i=0, size=0, cnt=0;
+		if(arr.length>arr2.length){
+			size=arr2.length;
+		}
+		else{
+			size=arr.length;
+		}
+		/*for(i=0; i<size;i++){
+			for(j=0; j<size; j++){
+				if(arr[i]==arr[j]){
+					cnt++;
+				}
+			}
+		}*/
+		for(i=0; i<size; i++){
+			if(isDuplicate(arr,arr2[i],size)){
+				cnt++;
+			}
+		}
+		return cnt;
+	}
+	/* 두 배열을 비교하여 맞은 갯수에 따라 등수를 출력하는 메소드
+	 * 1등 : 6, 2등 : 5+보너스번호, 3등 : 5, 4등 : 4, 5등: 3, 나머지 꽝
+	 */
+	public static void printRank(int arr[], int arr2[]){
+		int cnt= countCheck(arr, arr2);
+		boolean isBonus = false;
+		if(arr.length > arr2.length){
+			isBonus=isDuplicate(arr2, arr[arr.length-1], arr2.length);
+		}
+		else{
+			isBonus=isDuplicate(arr, arr2[arr2.length-1], arr.length);
+		}
+		if(cnt==6){
+			System.out.println("1등");
+		}
+		else if(cnt==5){
+			if(isBonus){
+				System.out.println("2등");
+			}
+			else{
+				System.out.println("3등");
+			}
+		}
+		else if(cnt==4){
+			System.out.println("4등");
+		}
+		else if(cnt==3){
+			System.out.println("5등");
+		}
+		else{
+			System.out.println("꽝");
+		}
+	}
+	public static void sort(int arr[], int size){
+		// 버블정렬 : 첫번재 숫자부터 옆의 숫자와 비교, 큰숫자를 뒤로 넘기면서 정렬
+		//       : j가 반복될때마다 큰 숫자가 뒤로 맨끝으로간다.
+		//       : i를 반복하여 정렬이 끝난숫자 이외에 남은 숫자중에서 다시 정렬한다.
+		for(int i=0; i<size-1; i++){
+			for(int j=0; j<size-1-i; j++){//반복횟수를 줄이기위해 j의 반복횟수에 -i를 추가
+				if(arr[j]>arr[j+1]){
+					int tmp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = tmp;
+				}//앞에 있는 수가 뒤의 수보다 크면 서로 교환
+			}
+		}
 	}
 }
